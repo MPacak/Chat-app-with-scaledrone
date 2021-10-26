1742fa7ef01faa5a2f1568f2b455f352075b8ec7
MPacak <maja.pacak1@outlook.com>
2021-10-26 14:43:01 +0200

    Update OnlineMembers.jsx

--- src/components/OnlineMembers.jsx
+++ src/components/OnlineMembers.jsx
@@ -1,11 +1,8 @@
-//online members for fantom user
-
 import React from "react";
 
 export default function OnlineMembers(props) {
   return (
     <li className="online-members" style={{ color: props.usercolor }}>
-      {props.number}
       {props.username}
     </li>
   );
